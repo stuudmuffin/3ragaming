fd21c54557633f4f207eb5042b63731971b502c1
articulating <samsail@live.com.au>
2016-10-06 20:00:39 +1100

    Seperate and add Enhanced Vanilla functionality.
    Rework of how the scenario works, adding score extended (with
    autolaunch), decoration remover and auto deconstruct.

---
+++ 3Ra_Enhanced_Vanilla 1.0.0/config.lua
@@ -0,0 +1,4 @@
+--Number of seconds between each announcement
+scenario.config.announcement_delay = 600;
+scenario.config.score_delay = 8 -- delay in seconds before hiding rocket score window (0 = never show)
+scenario.config.autolaunch_default = false -- default autolaunch option
---
+++ 3Ra_Enhanced_Vanilla 1.0.0/control.lua
@@ -0,0 +1,31 @@
+if not scenario then scenario = {} end
+if not scenario.config then scenario.config = {} end
+--config and event must be called first.
+require "config"
+require "event"
+require "admin"
+require "announcements"
+require "gravestone"
+require "rocket"
+require "autodeconstruct"
+require "undecorator"
+
+--Give starting items.
+function player_joined(event)
+  local player = game.players[event.player_index]
+  player.insert{name="iron-plate", count=8}
+  player.insert{name="pistol", count=1}
+  player.insert{name="firearm-magazine", count=20}
+  player.insert{name="burner-mining-drill", count = 2}
+  player.insert{name="stone-furnace", count = 2}
+end
+
+--Give player weapons after they die.
+function player_respawned(event)
+	local player = game.players[event.player_index]
+	player.insert{name="pistol", count=1}
+	player.insert{name="firearm-magazine", count=10}
+end
+
+Event.register(defines.events.on_player_created, player_joined)
+Event.register(defines.events.on_player_respawned, player_respawned)
