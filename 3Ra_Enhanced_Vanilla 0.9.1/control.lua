496707da038286488fd88fedf1b6eab309586d72
stuudmuffin <taytrho@gmail.com>
2016-10-07 15:51:16 -0600

    added brand to control.lua comments

--- 3Ra_Enhanced_Vanilla 0.9.1/control.lua
+++ 3Ra_Enhanced_Vanilla 0.9.1/control.lua
@@ -1,3 +1,5 @@
+--Enhanced Vanilla
+--A 3Ra Gaming compilation
 --New Player Join
 script.on_event(defines.events.on_player_created, function(event)
   local player = game.players[event.player_index]
