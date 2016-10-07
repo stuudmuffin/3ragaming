4de0db3fe6d90e9e1d3ddd097d7bf8ddc9b70ddc
stuudmuffin <taytrho@gmail.com>
2016-10-07 15:53:27 -0600

    Update config.lua

--- 3Ra_Enhanced_Vanilla 1.0.0/config.lua
+++ 3Ra_Enhanced_Vanilla 1.0.0/config.lua
@@ -1,3 +1,4 @@
+--A 3Ra Gaming compilation
 --Number of seconds between each announcement
 scenario.config.announcement_delay = 600;
 scenario.config.score_delay = 8 -- delay in seconds before hiding rocket score window (0 = never show)
